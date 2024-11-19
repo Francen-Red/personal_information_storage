6ffa90b43ab84c85791dacbbf9bdc2d2335c282d
Francen-Red <francenvenissered@gmail.com>
2024-11-19 16:04:49 +0800

    Defined invalid characters for address, added while-loop for address

--- personal_info_collector.py
+++ personal_info_collector.py
@@ -1,6 +1,8 @@
 personal_info = {}  # Initialize a dictionary to store personal information
 
-invalid_characters = [63, 123, 125, 91, 93, 36, 42, 37, 33, 38, 64, 94, 124, 92, 47, 60, 62, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57] # Use ASCII for format conditions on name
+name_invalid_chars = [63, 123, 125, 91, 93, 36, 42, 37, 33, 38, 64, 94, 124, 92, 47, 60, 62, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57] # Use ASCII for format conditions on name
+
+address_invalid_chars = [33, 42, 126, 96, 60, 62, 92, 124, 37, 34, 39, 36, 43]
 
 # Create a main loop for all personal information
 while True:
@@ -8,7 +10,7 @@ while True:
     while True:   # Loop for name
         input_name = input("Please input a name: ")
 
-        if any(ord(char) in invalid_characters for char in input_name):  # Check if the inputted name include invalid characters
+        if any(ord(char) in name_invalid_chars for char in input_name):  # Check if the inputted name include invalid characters for name
             print("Please input a valid name.")  # If yes, print an error message
             continue 
         else:
@@ -36,6 +38,15 @@ while True:
             print("Please input a valid marital status")   # Print an error message
         else:
             break   # Break the loop if the inputted marital status is valid
+
+    while True:  # Loop for Address
+        input_address= input("Please input an address: ")
+
+        if any(ord(char) in address_invalid_chars for char in input_address):  # Check if the inputted address include invalid characters for address
+            print("Please remove characters that are not allowed in an address.")  # If yes, print an error message
+            continue 
+        else:
+            break  # Break the loop if the inputted address is valid
 
 
 
